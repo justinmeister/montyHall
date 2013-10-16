4c21b26b4fc4614414acc9e6213f8ca9ff9fd3f4
Justin Armstrong <justinmeister@gmail.com>
2013-10-15 21:42:16 -0700

    Initial Commit

---
+++ montyHall.py
@@ -0,0 +1,48 @@
+#Monty Hall problem#
+
+import random
+
+door1 = 'Goat'
+door2 = 'Goat'
+door3 = 'Car'
+
+doorList = [door1, door2, door3]
+winList = []
+simulationTrys = 1000000
+
+
+## In this simulation, the 'contestant' always chooses the
+## first door.  Since the doors are randomized anyways, it
+## doesn't really matter, but it simplifies the problem.
+## Next, the program checks if the second door is a goat.
+## If so, in a gameshow, the host would reveal that door before
+## offering the switch.  Otherwise, he would reveal the third
+## door.
+
+for choices in range(simulationTrys):
+    random.shuffle(doorList)
+    if doorList[1] == 'Goat':
+        finalChoice = doorList[2]
+        if finalChoice == 'Car':
+            winList.append('win')
+    else:
+        finalChoice = doorList[1]
+        if finalChoice == 'Car':
+            winList.append('win')
+        
+
+
+
+wins = len(winList)
+
+winPercentage = (float(wins) / float(simulationTrys)) * 100
+
+
+
+print 'In a simulation of a million monty halls, the win percentagage is ' + str(winPercentage) + '%.'
+print 'Remember, always switch your choice!'
+    
+            
+        
+
+
